5f7b79c663f13276758eae7414afe05103eb9ca8
karan <kkadam58707@gmail.com>
2025-07-25 21:45:40 +0530

    using hashmap boi

--- TwoSum.java
+++ TwoSum.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TwoSum {
 
@@ -12,12 +14,40 @@ public class TwoSum {
         }
         return new int[] { -1, -1 };
     }
-
+    public int[] byPiyush(int [] arr,int target){
+        for(int i = 0;i<arr.length;i++){
+            int targetNum = target - arr[i];
+            for(int j = i+1;j<arr.length;j++){
+                if(targetNum==arr[j]){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{-1,-1};
+    }
+    public int[] ByPiyush(int[] arr , int target){
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0;i<arr.length;i++){
+            int targetnum = target - arr[i];
+            if(map.containsKey(targetnum)){
+                return new int[]{map.get(targetnum),i};
+            }
+            else{
+                map.put(arr[i], i);
+            }
+        }
+        return new int[]{-1,-1}; 
+    }
     public static void main(String[] args) {
-        int[] array = { 4, 6, 3 ,16};
+        int[] array = { 4, 8, 3 ,12};
         int goal = 20;
         TwoSum obj = new TwoSum();
-        int[] result = obj.ans(array, goal);
-        System.out.println(Arrays.toString(result));
+        // int[] result = obj.ans(array, goal);
+        // System.out.println(Arrays.toString(result));
+        // int[] result2 = obj.byPiyush(array, goal);
+        // System.out.println(Arrays.toString(result2));        
+        int[] result3= obj.byPiyush(array, goal);
+        System.out.println(Arrays.toString(result3));        
     }
-}
+    }
+
